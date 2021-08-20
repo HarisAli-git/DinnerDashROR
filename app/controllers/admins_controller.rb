eff3ae37c71f2834b7fad04731240732bee76a58
HarisAli-Bit <harisa73@gmail.com>
2021-08-20 16:14:50 +0500

    Controllers Updated

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,23 @@
+class AdminsController < ApplicationController
+    def index
+    end
+
+    def show
+        
+    end
+
+    def get_orders
+        
+    end
+
+    def get_items
+        
+    end
+
+    def get_users
+        
+    end
+
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     has_one :cart
 
-    validates :email, presence: true
+    email_valid = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true, format: { with: email_valid }, uniqueness: {case_sensitive: false}
     validates :name, presence: true
-    validates :username, presence: true
+    validates :username, length: {in: 2..32}
     validates :pass, presence: true, length: {minimum: 6, maximum: 8}
 end
